b3dd6f60ab51d42274789bb4488b8467cd0f4f45
pocketful <iveta.jac@gmail.com>
2024-06-06 15:47:10 +0300

    eslint imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,11 @@ module.exports = {
     react: {
       version: 'detect',
     },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
   extends: [
     'eslint:recommended',
@@ -14,17 +19,23 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'], 
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    '@typescript-eslint/consistent-type-definitions': 0,
+    'import/no-named-as-default-member': 0,
   },
 }
