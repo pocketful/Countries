48353ccb72903bc86d743df068307af2b73f0bba
pocketful <iveta.jac@gmail.com>
2024-06-07 16:04:11 +0300

    add filter

---
+++ src/components/CountriesPage/Filter/Filter.tsx
@@ -0,0 +1,37 @@
+import Button from '../../UI/Button/Button'
+import style from './Filter.module.scss'
+
+type activeFilters = {
+  filter: string
+  sort: string
+}
+
+type Props = {
+  activeFilters: activeFilters
+  onSortByName: () => void
+  onFilterSmallerThanLT: () => void
+  onFilterOceania: () => void
+}
+const Filter = ({ activeFilters, onSortByName, onFilterSmallerThanLT, onFilterOceania }: Props) => {
+  const { sort, filter } = activeFilters
+
+  return (
+    <header className={style.wrapper}>
+      <div className={style.sort}>
+        <Button onClick={onSortByName} isActive={sort === 'ByName'}>
+          Sort By Name
+        </Button>
+      </div>
+      <div className={style.filter}>
+        <Button onClick={onFilterSmallerThanLT} isActive={filter === 'Smaller'}>
+          Smaller Than LT
+        </Button>
+        <Button onClick={onFilterOceania} isActive={filter === 'Oceania'}>
+          Oceania
+        </Button>
+      </div>
+    </header>
+  )
+}
+
+export default Filter
